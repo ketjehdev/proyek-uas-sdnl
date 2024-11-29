ca57b22922637db7c8cc4a6253452ef747a755d9
Gompze <yosafat.de.santa@gmail.com>
2024-11-29 14:15:20 +0700

    feat:create linear correlation formula

--- src/main/java/com/kelompok2/util/LinearCorrelation.java
+++ src/main/java/com/kelompok2/util/LinearCorrelation.java
@@ -9,5 +9,35 @@ package com.kelompok2.util;
  * @author ASUS
  */
 public class LinearCorrelation {
-    
+
+    public static double calculateCorrelation(double[] x, double[] y) {
+        try {
+            if (x.length != y.length) {
+                throw new IllegalArgumentException("Arrays must have the same length.");
+            }
+
+            int n = x.length;
+            double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0, sumY2 = 0;
+
+            for (int i = 0; i < n; i++) {
+                sumX += x[i];
+                sumY += y[i];
+                sumXY += x[i] * y[i];
+                sumX2 += x[i] * x[i];
+                sumY2 += y[i] * y[i];
+            }
+
+            double numerator = n * sumXY - sumX * sumY;
+            double denominator = Math.sqrt((n * sumX2 - sumX * sumX) * (n * sumY2 - sumY * sumY));
+
+            if (denominator == 0) {
+                throw new ArithmeticException("Denominator is zero, correlation is undefined.");
+            }
+
+            return numerator / denominator;
+        } catch (IllegalArgumentException | ArithmeticException ex) {
+            System.out.println();
+        }
+    }
+
 }
